7c09a8dde0231012078d655ca23a715d86c84eb2
NightFall28 <95193970+NightFall28@users.noreply.github.com>
2022-08-08 20:12:00 -0400

    Create string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,7 @@
+# given the input format: lastname, id, GPA, firstname
+# output: id, firstname, lastname
+
+Str = "Random,1234,3.99,Soul"
+holder = Str.split(',')
+StrOutput = holder[1] + " " + holder[3] + " " + holder[0]
+print(StrOutput)
